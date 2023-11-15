92e3229d2e62de5d32ad0b05262776121be4fa69
bao148 <baovgpc06904@fpt.edu.vn>
2023-11-15 15:05:09 +0700

    dich lai site

--- site/pages/login.php
+++ site/pages/login.php
@@ -1,63 +1,61 @@
 <main>
-        <!-- Hero Area Start-->
-        <div class="slider-area ">
-            <div class="single-slider slider-height2 d-flex align-items-center">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xl-12">
-                            <div class="hero-cap text-center">
-                                <h2>Login</h2>
-                            </div>
+    <!-- Hero Area Start-->
+    <div class="slider-area ">
+        <div class="single-slider slider-height2 d-flex align-items-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="hero-cap text-center">
+                            <h2>Đăng nhập</h2>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- Hero Area End-->
-        <!--================login_part Area =================-->
-        <section class="login_part section_padding ">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-6 col-md-6">
-                        <div class="login_part_text text-center">
-                            <div class="login_part_text_iner">
-                                <h2>New to our Shop?</h2>
-                                <p>There are advances being made in science and technology
-                                    everyday, and a good example of this is the</p>
-                                <a href="#" class="btn_3">Create an Account</a>
-                            </div>
+    </div>
+    <!-- Hero Area End-->
+    <!--================login_part Area =================-->
+    <section class="login_part section_padding ">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-md-6">
+                    <div class="login_part_text text-center">
+                        <div class="login_part_text_iner">
+                            <h2>Lần đầu đến với chúng tôi?</h2>
+                            <p>Để có thể mua hàng cũng như chúng tôi có thể hỗ trợ quý khách được tốt hơn, xin vui lòng:</p>
+                            <a href="#" class="btn_3">Tạo tài khoản</a>
                         </div>
                     </div>
-                    <div class="col-lg-6 col-md-6">
-                        <div class="login_part_form">
-                            <div class="login_part_form_iner">
-                                <h3>Welcome Back ! <br>
-                                    Please Sign in now</h3>
-                                <form class="row contact_form" action="#" method="post" novalidate="novalidate">
-                                    <div class="col-md-12 form-group p_star">
-                                        <input type="text" class="form-control" id="name" name="name" value=""
-                                            placeholder="Username">
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="login_part_form">
+                        <div class="login_part_form_iner">
+                            <h3>Chào mừng quý khách trở lại ! <br></h3>
+                            <form class="row contact_form" action="#" method="post" novalidate="novalidate">
+                                <div class="col-md-12 form-group p_star">
+                                    <input type="text" class="form-control" id="name" name="name" value=""
+                                        placeholder="Tên đăng nhập">
+                                </div>
+                                <div class="col-md-12 form-group p_star">
+                                    <input type="password" class="form-control" id="password" name="password" value=""
+                                        placeholder="Mật khẩu">
+                                </div>
+                                <div class="col-md-12 form-group">
+                                    <div class="creat_account d-flex align-items-center">
+                                        <input type="checkbox" id="f-option" name="selector">
+                                        <label for="f-option">Lưu mật khẩu</label>
                                     </div>
-                                    <div class="col-md-12 form-group p_star">
-                                        <input type="password" class="form-control" id="password" name="password" value=""
-                                            placeholder="Password">
-                                    </div>
-                                    <div class="col-md-12 form-group">
-                                        <div class="creat_account d-flex align-items-center">
-                                            <input type="checkbox" id="f-option" name="selector">
-                                            <label for="f-option">Remember me</label>
-                                        </div>
-                                        <button type="submit" value="submit" class="btn_3">
-                                            log in
-                                        </button>
-                                        <a class="lost_pass" href="#">forget password?</a>
-                                    </div>
-                                </form>
-                            </div>
+                                    <button type="submit" value="submit" class="btn_3">
+                                        Đăng nhập
+                                    </button>
+                                    <a class="lost_pass" href="#">Quên mật khẩu?</a>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
-        <!--================login_part end =================-->
-    </main>
+        </div>
+    </section>
+    <!--================login_part end =================-->
+</main>
