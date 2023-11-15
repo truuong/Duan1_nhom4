dec50871f08ac691a26050bf10e41955e3c17707
cuonghcpc04375 <94127618+cuonghcpc04375@users.noreply.github.com>
2023-11-15 09:15:45 +0700

    sửa giao diện detailOrder

--- admin/orders/detailOrder.php
+++ admin/orders/detailOrder.php
@@ -1,6 +1,6 @@
 <?php include '../components/header.php';?>
 
-    <div class="container">
+    
         <main class="main">
             <div class="row">
                 <div class="col-sm-3 col-md-3 col-lg-3">
