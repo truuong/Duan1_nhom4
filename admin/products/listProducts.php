a12f60af4696fd3f0ad47202d996d28cb610d3ad
truong <truongnvpc04277@fpt.edu.vn>
2023-11-23 20:36:02 +0700

    them,sua,xoa ADMIN

--- admin/products/listProducts.php
+++ admin/products/listProducts.php
@@ -12,28 +12,35 @@
                             <th>Hình ảnh</th>
                             <th>Giảm giá</th>
                             <th>Giá</th>
-                            <th>Số lượng</th>
                             <th>Danh mục</th>
                             <th>Quản lý</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Đồng hồ Gucci</td>
-                            <td><img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSJv_06krOqCNisodkCNGkfGSb17ukdCotgmzXtaXhdy2A_1HhCnaLMg5tkmgRlfbijiQzYtfl_vY9UOvjUAubDPmYYarUMJDNMiU_cSTV_uVPfwc1ivQoGWg&usqp=CAE"
-                                        width="50px" height="50px" alt="">
-                            </td>
-                            <td>30%</td>
-                            <td>10,000,000VNĐ</td>
-                            <td>110</td>
-                            <td>Đồng hồ</td>
+                <?php
 
-                            <td>
-                                <a href="index.php?action=products&sub_action=edit" class="btn btn-primary">Sửa</a>
-                                <a href="" class="btn btn-danger">Xóa</a>
-                            </td>
-                        </tr>
+                        foreach ($listsp as $sp) {
+                            extract($sp);
+                            $suasp = "index.php?sub_action=edit&id" . $id;
+
+
+                            echo '<tr>
+                                    <td>' . $id . '</td>
+                                    <td>' . $name . '</td>
+                                    <td><img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSJv_06krOqCNisodkCNGkfGSb17ukdCotgmzXtaXhdy2A_1HhCnaLMg5tkmgRlfbijiQzYtfl_vY9UOvjUAubDPmYYarUMJDNMiU_cSTV_uVPfwc1ivQoGWg&usqp=CAE"
+                                        width="50px" height="50px" alt="">
+                                    </td>
+                                    <td>' . $price_sale . '%</td>
+                                    <td>' . $price . 'VNĐ</td>
+                                    <td>' . $cate_id . '</td>
+
+                                    <td>
+                                    <a href="' . $suasp . '" class="btn btn-primary">Sửa</a>
+                                    <a href="" class="btn btn-danger">Xóa</a>
+                                    </td>
+                                </tr>';
+                        }
+                ?>
                     </tbody>
                 </table>
             </div>
---
+++ dao/categories.php
@@ -0,0 +1,64 @@
+<?php
+require_once 'pdo.php';
+
+/**
+ * Thêm loại mới
+ * @param String $name là tên loại
+ * @throws PDOException lỗi thêm mới
+ */
+function loai_insert($name){
+    $sql = "INSERT INTO loai(name) VALUES('$name')";
+    pdo_execute($sql);
+}
+/**
+ * Cập nhật tên loại
+ * @param int $id  là mã loại cần cập nhật
+ * @param String $name là tên loại mới
+ * @throws PDOException lỗi cập nhật
+ */
+function loai_update($id , $name){
+    $sql = "UPDATE loai SET name='$name' WHERE id ='$id '";
+    pdo_execute($sql);
+}
+/**
+ * Xóa một hoặc nhiều loại
+ * @param mix $id  là mã loại hoặc mảng mã loại
+ * @throws PDOException lỗi xóa
+ */
+function loai_delete($id ){
+    $sql = "DELETE FROM loai WHERE id ='$id '";
+    pdo_execute($sql);
+    
+}
+/**
+ * Truy vấn tất cả các loại
+ * @return array mảng loại truy vấn được
+ * @throws PDOException lỗi truy vấn
+ */
+function loai_select_all(){
+    $sql = "SELECT * FROM loai ORDER BY id  ASC";
+    $listloai = pdo_query($sql);
+    return $listloai;
+}
+/**
+ * Truy vấn một loại theo mã
+ * @param int $id  là mã loại cần truy vấn
+ * @return array mảng chứa thông tin của một loại
+ * @throws PDOException lỗi truy vấn
+ */
+function loai_select_by_id($id ){
+    $sql = "SELECT * FROM loai WHERE id =$id ";
+    $dm = pdo_query_one($sql);
+    return $dm;
+
+}
+/**
+ * Kiểm tra sự tồn tại của một loại
+ * @param int $id  là mã loại cần kiểm tra
+ * @return boolean có tồn tại hay không
+ * @throws PDOException lỗi truy vấn
+ */
+function loai_exist($id ){
+    $sql = "SELECT count(*) FROM loai WHERE id =$id ";
+    return pdo_query_value($sql, $id ) > 0;
+}
