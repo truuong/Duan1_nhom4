e8215070b8328f0d28a8aa97fe2a9b961c6b0460
ngogiangnampc04380 <119033482+ngogiangnampc04380@users.noreply.github.com>
2023-11-24 14:50:59 +0700

    awdawd

--- admin/index.php
+++ admin/index.php
@@ -4,29 +4,29 @@ include "components/header.php";
 
 
 if (isset($_GET['action'])) {
-  $action = $_GET['action'];
+    $action = $_GET['action'];
 
-  switch ($action) {
-      case 'categories':
-          if (isset($_GET['sub_action'])) {
-              $subAction = $_GET['sub_action'];
-              switch ($subAction) {
-                  case 'edit':
-                      include "categories/editCategory.php";
-                      break;
-                  case 'add':
-                      include "categories/addCategory.php";
-                      break;
-                  default:
-                      include "categories/listCategories.php";
-                      break;
-              }
-          } else {
-              include "categories/listCategories.php";
-          }
-          break;
+    switch ($action) {
+        case 'categories':
+            if (isset($_GET['sub_action'])) {
+                $subAction = $_GET['sub_action'];
+                switch ($subAction) {
+                    case 'edit':
+                        include "categories/editCategory.php";
+                        break;
+                    case 'add':
+                        include "categories/addCategory.php";
+                        break;
+                    default:
+                        include "categories/listCategories.php";
+                        break;
+                }
+            } else {
+                include "categories/listCategories.php";
+            }
+            break;
 
-          case 'products':
+        case 'products':
             if (isset($_GET['sub_action'])) {
                 $subAction = $_GET['sub_action'];
                 switch ($subAction) {
@@ -44,7 +44,7 @@ if (isset($_GET['action'])) {
                 include "products/listProducts.php";
             }
             break;
-        
+
         case 'users':
             if (isset($_GET['sub_action'])) {
                 $subAction = $_GET['sub_action'];
@@ -60,7 +60,7 @@ if (isset($_GET['action'])) {
                 include "users/listUsers.php";
             }
             break;
-        
+
         case 'comments':
             if (isset($_GET['sub_action'])) {
                 $subAction = $_GET['sub_action'];
@@ -76,7 +76,7 @@ if (isset($_GET['action'])) {
                 include "comments/comments.php";
             }
             break;
-        
+
         case 'orders':
             if (isset($_GET['sub_action'])) {
                 $subAction = $_GET['sub_action'];
@@ -92,16 +92,16 @@ if (isset($_GET['action'])) {
                 include "orders/listOrders.php";
             }
             break;
-        
 
-      default:
-          include "components/home.php";
-          break;
-  }
+
+        default:
+            include "components/home.php";
+            break;
+    }
 } else {
-  include "components/home.php";
+    include "components/home.php";
 }
 
- 
+
 include "components/footer.php";
 ?>
